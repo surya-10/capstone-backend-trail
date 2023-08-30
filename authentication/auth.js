9b947c21a9d65bb050db1bc42b83315f4557fdc2
surya-10 <sp659151@gmail.com>
2023-08-30 13:19:09 +0530

    done

--- authentication/auth.js
+++ authentication/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 export function isAuth(req, res, next){
     let token = req.headers["auth-token"];
     if(!token){
-        return res.status(400).send({message:false});
+        return res.status(400).send({response:false, message:"Authentication failed"});
     }
     let result = jwt.verify(token, process.env.SECRET_KEY);
     next();
