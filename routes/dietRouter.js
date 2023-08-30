cde83ec577d15bfaa203046619853d21bfa7051c
surya-10 <sp659151@gmail.com>
2023-08-30 13:02:39 +0530

    done

---
+++ routes/dietRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+let dietRouter = express.Router();
+
+dietRouter.post("/diet-app", (req, res)=>{
+    try {
+        let {age, height, weight} = req.body;
+        if(age<=2){
+            return res.status(400).json({messsgae:"age should be greater than 2", response:false})
+        }
+        let heightInMeter = height/100;
+        let bmi = weight/(heightInMeter*heightInMeter);
+        return res.status(200).json({messsgae:"success", response:true});
+    } catch (error) {
+        return res.status(500).send("server error");
+    }
+})
