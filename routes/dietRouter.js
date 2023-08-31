427a009fdcd2b2f5d5fe4880801f7db258810f9f
surya-10 <sp659151@gmail.com>
2023-08-31 20:34:34 +0530

    done

--- routes/dietRouter.js
+++ routes/dietRouter.js
@@ -2,17 +2,18 @@ import express from "express";
 
 let dietRouter = express.Router();
 
-dietRouter.post("/diet-app", (req, res)=>{
+dietRouter.post("/diet-app", async (req, res)=>{
     try {
-        
+        // console.log(req.body)
         let {age, height, weight} = req.body;
-        console.log(req,body)
+        // console.log(req.body)
+        
         if(age<=2){
-            return res.status(400).json({messsgae:"age should be greater than 2", response:false})
+            return res.status(400).json({message:"age should be greater than 2", response:false, token:"success"})
         }
         let heightInMeter = height/100;
         let bmi = weight/(heightInMeter*heightInMeter);
-        return res.status(200).json({messsgae:"success", response:true});
+        return res.status(200).json({message:"success", response:true, userBmi:bmi.toFixed(2), token:true});
     } catch (error) {
         return res.status(500).send("server error");
     }
